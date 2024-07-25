ebca47e06c0f8d31dc8cd97469a9c43ce5a71a31
Wilson <wwilson008@gmail.com>
2024-07-26 06:01:01 +0800

    general_operator add complex key in redis_operate[update]

--- general_operator/app/redis_db/redis_operate.py
+++ general_operator/app/redis_db/redis_operate.py
@@ -1,64 +1,10 @@
 import json
 from typing import Any
+from .function import OperateFunction
 
 import redis
 from fastapi.encoders import jsonable_encoder
-
-
-class OperateFunction:
-    @staticmethod
-    def write_main_table(sql_data_list: list, schemas_model, set_mapping: dict):
-        result = []
-        for sql_data in sql_data_list:
-            row = schemas_model(**jsonable_encoder(sql_data))
-            value = row.json()
-            set_mapping[getattr(row, "id")] = value
-            result.append(row)
-        return result
-
-    @staticmethod
-    def write_index_table(sql_data_list: list, schemas_model, set_mapping: dict, key: str,
-                          table_name: str, r: redis.Redis):
-        result = []
-        # 取得初始資料
-        key_list = [getattr(schemas_model(**jsonable_encoder(sql_data)), key) for sql_data in sql_data_list]
-        # key_list = [sql_data[key] for sql_data in sql_data_list]
-        r_data = r.hmget(table_name, key_list)
-        set_mapping.update({x[0]: x[1] for x in zip(key_list, r_data) if x[1] is not None})
-        # sql type 是 json list的情況
-        if isinstance(getattr(schemas_model(**jsonable_encoder(sql_data_list[0])), key), list):
-            for sql_data in sql_data_list:
-                row = schemas_model(**jsonable_encoder(sql_data))
-                for item in getattr(row, key):
-                    original_data = set_mapping.get(item, None)
-                    if original_data:
-                        value_list = json.loads(original_data)
-                        if row.id not in value_list:
-                            value_list.append(row.id)
-                        set_mapping[item] = json.dumps(value_list)
-                    else:
-                        set_mapping[item] = json.dumps([row.id])
-                result.append(row)
-
-        # sql type 是單一值的情況
-        else:
-            for sql_data in sql_data_list:
-                row = schemas_model(**jsonable_encoder(sql_data))
-                original_data = set_mapping.get(getattr(row, key), None)
-                if original_data:
-                    value_list = json.loads(original_data)
-                    if row.id not in value_list:
-                        value_list.append(row.id)
-                    set_mapping[getattr(row, key)] = json.dumps(value_list)
-                else:
-                    set_mapping[getattr(row, key)] = json.dumps([row.id])
-                result.append(row)
-
-        return result
-
-
-
-
+from enum import Enum
 
 class RedisOperate(OperateFunction):
     def __init__(self, redis_db: redis.Redis, exc):
@@ -94,39 +40,6 @@ class RedisOperate(OperateFunction):
             result = self.write_main_table(sql_data_list, schemas_model, set_mapping)
         else:
             result = self.write_index_table(sql_data_list, schemas_model, set_mapping, key, table_name, self.redis)
-        # for sql_data in sql_data_list:
-        #     row = schemas_model(**jsonable_encoder(sql_data))
-        #     # 寫入主表
-        #     if key == "id":
-        #         value = row.json()
-        #         set_mapping[getattr(row, key)] = value
-        #     # 寫入附表(index table)
-        #     else:
-        #         # sql type 是 json list的情況
-        #         if isinstance(getattr(row, key), list):
-        #             for item in getattr(row, key):
-        #                 original_data = set_mapping.get(item, None)
-        #                 if original_data:
-        #                     value_list = json.loads(original_data)
-        #                     value_list.append(row.id)
-        #                     set_mapping[item] = json.dumps(value_list)
-        #                 else:
-        #                     set_mapping[item] = json.dumps([row.id])
-        #         # sql type 是單一值的情況
-        #         else:
-        #             item = getattr(row, key)
-        #             # key可能為空值得情況
-        #             if item is not None:
-        #                 original_data = set_mapping.get(item, None)
-        #                 if original_data:
-        #                     value_list = json.loads(original_data)
-        #                     value_list.append(row.id)
-        #                     set_mapping[item] = json.dumps(value_list)
-        #                 else:
-        #                     set_mapping[item] = json.dumps([row.id])
-        #
-        #     result.append(row)
-        # 統一set data
         if set_mapping:
             self.redis.hset(table_name, mapping=set_mapping)
         return result
@@ -182,27 +95,65 @@ class RedisOperate(OperateFunction):
             # 刪除主表
             if key == "id":
                 p.hdel(table_name, getattr(row, key))
+                continue
+
             # 刪除附表(index table)
-            elif not update_list or getattr(update_dict.get(row.id, None), key, None) is not None:
+            is_complex_key = False
+            has_key = False
+            key_list = []
+            if len(key.split("__")) > 1:
+                is_complex_key = True
+                key_list = key.split("__")
+
+            #  判斷更新資料有沒有此key
+            if is_complex_key:
+                key_list = key.split("__")
+                for k in key_list:
+                    if getattr(update_dict.get(row.id, None), k, None) is not None:
+                        has_key = True
+                        break
+            else:
+                if getattr(update_dict.get(row.id, None), key, None) is not None:
+                    has_key = True
+
+            if not update_list or has_key:
+                # key是complex的情況
+                if is_complex_key:
+                    complex_value = ""
+                    for k in key_list:
+                        value = getattr(row, k)
+                        if value is not None:
+                            if isinstance(value, Enum):
+                                complex_value += value.value
+                            else:
+                                complex_value += str(value)
+                    id_list = json.loads(self.redis.hget(table_name, complex_value))
+                    id_list.remove(row.id)
+                    if not id_list:
+                        p.hdel(table_name, complex_value)
+                    else:
+                        p.hset(table_name, complex_value, json.dumps(id_list))
+
                 # sql type 是 json list的情況
-                if isinstance(getattr(row, key), list):
-                    for item in getattr(row, key):
-                        value_list = json.loads(self.redis.hget(table_name, item))
-                        value_list.remove(row.id)
-                        if not value_list:
-                            p.hdel(table_name, item)
+                elif isinstance(getattr(row, key), list):
+                    for value in getattr(row, key):
+                        id_list = json.loads(self.redis.hget(table_name, value))
+                        id_list.remove(row.id)
+                        if not id_list:
+                            p.hdel(table_name, value)
                         else:
-                            p.hset(table_name, item, json.dumps(value_list))
+                            p.hset(table_name, value, json.dumps(id_list))
+
                 # sql type 是單一值的情況
                 else:
-                    item = getattr(row, key)
+                    value = getattr(row, key)
                     # key可能為空值得情況
-                    if item is not None:
-                        value_list = json.loads(self.redis.hget(table_name, item))
-                        value_list.remove(row.id)
-                        if not value_list:
-                            p.hdel(table_name, item)
+                    if value is not None:
+                        id_list = json.loads(self.redis.hget(table_name, value))
+                        id_list.remove(row.id)
+                        if not id_list:
+                            p.hdel(table_name, value)
                         else:
-                            p.hset(table_name, item, json.dumps(value_list))
+                            p.hset(table_name, value, json.dumps(id_list))
         p.execute()
         return "Ok"
