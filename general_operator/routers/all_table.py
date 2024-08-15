b2d98feadf9d7d431700801f6cb617b51d232c89
Wilson <wwilson008@gmail.com>
2024-08-15 16:40:29 +0800

    general operate[update]

--- general_operator/routers/all_table.py
+++ general_operator/routers/all_table.py
@@ -11,19 +11,21 @@ from ..app.influxdb.influxdb import InfluxDB
 
 class AllTableRouter(AllTableOperator):
     def __init__(self, module, redis_db: redis.Redis, influxdb: InfluxDB, exc,
-                 db_session: sessionmaker, is_initial=False):
+                 db_session: sessionmaker, is_initial=False, include_in_schema=False):
         self.redis_db = redis_db
         self.influxdb = influxdb
         self.exc = exc
         self.db_session = db_session
         self.is_initial = is_initial
+        self.include_in_schema = include_in_schema
         AllTableOperator.__init__(self, module, redis_db, influxdb, exc)
 
     def create(self):
         router = APIRouter(
             prefix=f"/all_operator",
-            tags=[f"all_operator"],
-            dependencies=[]
+            tags=[f"All Operator"],
+            dependencies=[],
+            include_in_schema=self.include_in_schema,
         )
         @router.on_event("startup")
         async def task_startup_event():
